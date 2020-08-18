811a37108d7cbed38a0be80cbaf4645e88585cea
Oscar Garcia <you@example.com>
2020-08-18 11:10:47 -0700

    updating stripe integration and updating favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+
+  const publishableKey =
+    "pk_test_51HGBXiCGp1qn4gZ7onUUWeG1ri0uOKsKiOg9HW99LbRxUfFeeEVzHdE8men05zsYHPswWzPWdqZBwhKtYccHs27400CJ29JqZy";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Succesful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="crwn Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
